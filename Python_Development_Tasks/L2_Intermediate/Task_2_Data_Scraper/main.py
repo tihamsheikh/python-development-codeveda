e861ef37a50c364e0891b9ca1a7f154a976dcc93
Fardin Sheikh Tiham <tihamsheikhone@gmail.com>
2026-03-05 04:32:32 +0600

    Add files via upload

--- Python_Development_Tasks/L2_Intermediate/Task_2_Data_Scraper/main.py
+++ Python_Development_Tasks/L2_Intermediate/Task_2_Data_Scraper/main.py
@@ -1,22 +1,26 @@
 import requests 
 import csv
+import os
 from bs4 import BeautifulSoup 
 from fake_useragent import UserAgent 
 import time
 from pathlib import Path
 from datetime import datetime
+from dotenv import load_dotenv 
+
+load_dotenv()
 
 
 # Define directory and file path
 directory = Path("data")
-file_path = directory / f"{datetime.now().strftime('%Y-%m-%d')}_scraped_data.csv"
-print(file_path)
+file_path = directory / f"{datetime.now().strftime('%Y-%m-%d-%H-%M')}_scraped_data.csv"
+# print(file_path)
 
-proxie_auth = "xigkfarw-rotate:pwco0u3cbqug@163.223.21.221:9999"
+proxie_auth = os.getenv("proxie_auth")
 
 url = "https://www.kalerkantho.com/special/recent"
 
-print(UserAgent().random)
+
 header = {
     "User-Agent": UserAgent().random,
     "Connection": "keep-alive",
@@ -30,7 +34,7 @@ proxie = {
 session = requests.Session()
 time.sleep(2)
 response = session.get(url=url, proxies=proxie, headers=header)
-print(response.status_code, "hi")
+# print(response.status_code, "hi")
 
 result = response.text 
 
@@ -38,11 +42,14 @@ soup = BeautifulSoup(result, "html.parser")
 titles = soup.find_all("h5", class_="card-title")
 source = soup.find_all("span", class_="text-dangers")
 
-for title in titles:
-    print(title.text.strip())
+# for title in titles:
+#     print(title.text.strip())
 
 with open(file_path, "w") as f:
     writer = csv.writer(f)
     writer.writerow(["Title", "Source"])
     for i, title in enumerate(titles):
         writer.writerow([title.text.strip(), source[i].text.strip() if i < len(source) else "N/A"])
+
+
+print(f"Data has been successfully scraped and saved to {file_path}")
